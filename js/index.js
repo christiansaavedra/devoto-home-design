27f3b704de19086a70a24d87a2b55839e461ca06
Christian-Saavedra <saavedra.christian.ok@gmail.com>
2019-12-17 00:48:48 -0300

    agrego btn whatsapp, anchors, etc

--- js/index.js
+++ js/index.js
@@ -2,8 +2,6 @@ let zoomBg = document.querySelector(".zoom-bg");
 
 function carousel(a, b) {
   zoomBg.style.display = "flex";
-  console.log(a);
-
   zoomBg.innerHTML = `
   <div class="zoom-content">
   <div id="carouselExampleControls" class="carousel" data-ride="carousel" data-interval="false">
@@ -28,6 +26,35 @@ function carousel(a, b) {
 </div> `;
 }
 
+function carouselPlanos(a, b, c) {
+  zoomBg.style.display = "flex";
+  zoomBg.innerHTML = `
+  <div class="zoom-content">
+  <div id="carouselExampleControls" class="carousel" data-ride="carousel" data-interval="false">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="zoom-img" src="${a}" >
+    </div>
+    <div class="carousel-item">
+      <img class="zoom-img" src="${b}" >
+    </div>
+    <div class="carousel-item">
+    <img class="zoom-img" src="${c}" >
+  </div>
+   
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"  style="background-color: black"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true" style="background-color: black"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+</div> `;
+}
+
 function zoomClose() {
   if (event.target !== event.currentTarget) {
     return;
@@ -35,3 +62,39 @@ function zoomClose() {
     zoomBg.style.display = "none";
   }
 }
+
+$(document).ready(function() {
+  $("anchor-proyecto").on("click", function(event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $("html, body").animate(
+        {
+          scrollTop: $(hash).offset().top
+        },
+        800,
+        function() {
+          window.location.hash = hash;
+        }
+      );
+    }
+  });
+});
+
+$(document).ready(function() {
+  $("anchor-contacto").on("click", function(event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $("html, body").animate(
+        {
+          scrollTop: $(hash).offset().top
+        },
+        800,
+        function() {
+          window.location.hash = hash;
+        }
+      );
+    }
+  });
+});
