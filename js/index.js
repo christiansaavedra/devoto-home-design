4f20e7fa1dbe1086db1f87614bf8a717b2453cd2
Christian-Saavedra <saavedra.christian.ok@gmail.com>
2019-12-18 00:21:41 -0300

    agrego footer, logos

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 let zoomBg = document.querySelector(".zoom-bg");
+let zoomBgPlanos = document.querySelector(".zoom-bg-planos");
 
 function carousel(a, b) {
   zoomBg.style.display = "flex";
@@ -27,28 +28,28 @@ function carousel(a, b) {
 }
 
 function carouselPlanos(a, b, c) {
-  zoomBg.style.display = "flex";
-  zoomBg.innerHTML = `
+  zoomBgPlanos.style.display = "flex";
+  zoomBgPlanos.innerHTML = `
   <div class="zoom-content">
   <div id="carouselExampleControls" class="carousel" data-ride="carousel" data-interval="false">
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img class="zoom-img" src="${a}" >
+      <img class="zoom-img-planos" src="${a}" >
     </div>
     <div class="carousel-item">
-      <img class="zoom-img" src="${b}" >
+      <img class="zoom-img-planos" src="${b}" >
     </div>
     <div class="carousel-item">
-    <img class="zoom-img" src="${c}" >
+    <img class="zoom-img-planos" src="${c}" >
   </div>
    
   </div>
   <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"  style="background-color: black"></span>
+    <span class="carousel-control-prev-icon" aria-hidden="true" ></span>
     <span class="sr-only">Previous</span>
   </a>
   <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true" style="background-color: black"></span>
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
     <span class="sr-only">Next</span>
   </a>
 </div>
@@ -60,6 +61,7 @@ function zoomClose() {
     return;
   } else {
     zoomBg.style.display = "none";
+    zoomBgPlanos.style.display = "none";
   }
 }
 
