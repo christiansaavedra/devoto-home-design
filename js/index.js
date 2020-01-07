2962595b1fc0cc96559febb399b0f6f4aef2f9b2
Christian-Saavedra <saavedra.christian.ok@gmail.com>
2020-01-07 00:32:10 -0300

    agrego media queries

--- js/index.js
+++ js/index.js
@@ -1,9 +1,11 @@
 let zoomBg = document.querySelector(".zoom-bg");
 let zoomBgPlanos = document.querySelector(".zoom-bg-planos");
 
-function carousel(a, b) {
-  zoomBg.style.display = "flex";
-  zoomBg.innerHTML = `
+window.addEventListener("resize", function() {
+  if (window.innerWidth > 568) {
+    function carousel(a, b, c) {
+      zoomBg.style.display = "flex";
+      zoomBg.innerHTML = `
   <div class="zoom-content">
   <div id="carouselExampleControls" class="carousel" data-ride="carousel" data-interval="false">
   <div class="carousel-inner">
@@ -13,6 +15,15 @@ function carousel(a, b) {
     <div class="carousel-item">
       <img class="zoom-img" src="${b}" >
     </div>
+    
+    ${
+      c
+        ? `<div class="carousel-item asd">
+        <img class="zoom-img" src="${c}" >
+      </div>`
+        : ""
+    }
+    
    
   </div>
   <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
@@ -25,8 +36,8 @@ function carousel(a, b) {
   </a>
 </div>
 </div> `;
-}
-
+    }
+    /* 
 function carouselPlanos(a, b, c) {
   zoomBgPlanos.style.display = "flex";
   zoomBgPlanos.innerHTML = `
@@ -34,18 +45,16 @@ function carouselPlanos(a, b, c) {
   <div id="carouselExampleControls" class="carousel" data-ride="carousel" data-interval="false">
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img class="zoom-img-planos" src="${a}" >
+      <img class="zoom-img" src="${a}" >
     </div>
     <div class="carousel-item">
-      <img class="zoom-img-planos" src="${b}" >
+      <img class="zoom-img" src="${b}" >
     </div>
-    <div class="carousel-item">
-    <img class="zoom-img-planos" src="${c}" >
-  </div>
+    
    
   </div>
   <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true" ></span>
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
     <span class="sr-only">Previous</span>
   </a>
   <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
@@ -54,17 +63,18 @@ function carouselPlanos(a, b, c) {
   </a>
 </div>
 </div> `;
-}
+} */
 
-function zoomClose() {
-  if (event.target !== event.currentTarget) {
-    return;
-  } else {
-    zoomBg.style.display = "none";
-    zoomBgPlanos.style.display = "none";
+    function zoomClose() {
+      if (event.target !== event.currentTarget) {
+        return;
+      } else {
+        zoomBg.style.display = "none";
+        zoomBgPlanos.style.display = "none";
+      }
+    }
   }
-}
-
+});
 $(document).ready(function() {
   $("anchor-proyecto").on("click", function(event) {
     if (this.hash !== "") {
@@ -85,6 +95,24 @@ $(document).ready(function() {
 
 $(document).ready(function() {
   $("anchor-contacto").on("click", function(event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $("html, body").animate(
+        {
+          scrollTop: $(hash).offset().top
+        },
+        800,
+        function() {
+          window.location.hash = hash;
+        }
+      );
+    }
+  });
+});
+
+$(document).ready(function() {
+  $("anchor-nosotros").on("click", function(event) {
     if (this.hash !== "") {
       event.preventDefault();
       var hash = this.hash;
